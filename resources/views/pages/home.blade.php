48377d8e8c9201d73aab099bca1c326be3c75dc6
Alissin82 <92631352+Alissin82@users.noreply.github.com>
2026-02-11 00:42:55 +0330

    feat: add a simple home page with livewire

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,14 @@
+<?php
+
+use Livewire\Attributes\Title;
+use Livewire\Component;
+
+new class extends Component {
+    #[Title('خانه')]
+};
+?>
+
+<div class="d-flex flex-column justify-content-center align-items-center p-3 tw:h-screen">
+    <h1 class="font-weight-bold">سلام دنیا</h1>
+    <span>زمستان 1404</span>
+</div>
