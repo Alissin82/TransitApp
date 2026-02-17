d7e97e1b7e471adeb6d2638c5e5dfd231c3d9dad
Alissin82 <92631352+Alissin82@users.noreply.github.com>
2026-02-17 19:55:23 +0330

    build: use mdb-ui-kit instead mdbootstrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,10 @@
 import axios from 'axios';
+import * as mdb from 'mdb-ui-kit';
+import { initMDB } from 'mdb-ui-kit';
+
 window.axios = axios;
+
+// TODO: consider tree-shaking later to improve performance
+initMDB(mdb);
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
