8e6e1811e5b3a6ebab8a810a702e2a9dbf05baa2
Alissin82 <92631352+Alissin82@users.noreply.github.com>
2026-02-17 20:13:57 +0330

    style: remove white-spaces from app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,70 +1,70 @@
 <!DOCTYPE html>
 <html lang="fa" dir="rtl">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? config('app.name') }}</title>
+    <title>{{ $title ?? config('app.name') }}</title>
 
-        <!-- Favicon -->
-        <link rel="icon" href="{{ asset('favicon.svg') }}">
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('favicon.svg') }}">
 
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-        @livewireStyles
-    </head>
-    <body class="tw:h-screen d-flex flex-column">
-        <header>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                <div class="container-fluid">
-                    <!-- Toggle button -->
-                    <button
-                            data-mdb-collapse-init
-                            class="navbar-toggler"
-                            type="button"
-                            data-mdb-target="#navbarSupportedContent"
-                            aria-controls="navbarSupportedContent"
-                            aria-expanded="false"
-                            aria-label="Toggle navigation"
-                    >
-                        <i class="fas fa-bars"></i>
-                    </button>
+    @livewireStyles
+</head>
+<body class="tw:h-screen d-flex flex-column">
+<header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <!-- Toggle button -->
+            <button
+                    data-mdb-collapse-init
+                    class="navbar-toggler"
+                    type="button"
+                    data-mdb-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+            >
+                <i class="fas fa-bars"></i>
+            </button>
 
-                    <!-- Collapsible wrapper -->
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <!-- Navbar brand -->
-                        <a class="navbar-brand mt-2 mt-lg-0 d-flex align-items-center gap-2" href="javascript:void(0)">
-                            <img src="{{ asset('logo.svg') }}" alt="TransitApp Logo" width="30" height="30">
-                            {{ config('app.name') }}
-                        </a>
-                        <!-- Left links -->
-                        <ul class="navbar-nav me-auto">
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('dashboard') }}">داشبورد</a>
-                            </li>
-                        </ul>
-                        <!-- Left links -->
-                    </div>
-                    <!-- Collapsible wrapper -->
-
-                    <!-- End elements -->
-                    <div class="d-flex align-items-center">
-                    </div>
-                    <!-- End elements -->
-                </div>
-            </nav>
-        </header>
-
-        <main class="container d-flex flex-column flex-fill tw:*:flex-1">
-            {{ $slot }}
-        </main>
-
-        <footer>
-            <div class="container-fluid text-center p-3 bg-body-tertiary">
-                تمام حقوق این برنامه محفوظ می‌باشد.
+            <!-- Collapsible wrapper -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Navbar brand -->
+                <a class="navbar-brand mt-2 mt-lg-0 d-flex align-items-center gap-2" href="javascript:void(0)">
+                    <img src="{{ asset('logo.svg') }}" alt="TransitApp Logo" width="30" height="30">
+                    {{ config('app.name') }}
+                </a>
+                <!-- Left links -->
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('dashboard') }}">داشبورد</a>
+                    </li>
+                </ul>
+                <!-- Left links -->
             </div>
-        </footer>
+            <!-- Collapsible wrapper -->
 
-        @livewireScripts
-    </body>
+            <!-- End elements -->
+            <div class="d-flex align-items-center">
+            </div>
+            <!-- End elements -->
+        </div>
+    </nav>
+</header>
+
+<main class="container d-flex flex-column flex-fill tw:*:flex-1">
+    {{ $slot }}
+</main>
+
+<footer>
+    <div class="container-fluid text-center p-3 bg-body-tertiary">
+        تمام حقوق این برنامه محفوظ می‌باشد.
+    </div>
+</footer>
+
+@livewireScripts
+</body>
 </html>
