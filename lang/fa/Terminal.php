066716ab2c2e1a186c92f7d803769ddfd12b4b41
Alissin82 <92631352+Alissin82@users.noreply.github.com>
2026-02-20 19:53:14 +0330

    lang: organize Terminal.php

--- lang/fa/Terminal.php
+++ lang/fa/Terminal.php
@@ -1,25 +1,31 @@
 <?php
 
 return [
+    // Nouns
+    'Singular' => 'پایانه',
     'Plural' => 'پایانه‌ها',
 
+    // Phrases
     'Manage Terminals' => 'مدیریت پایانه‌ها',
     'New Terminal' => 'پایانه جدید',
     'Edit Terminal' => 'ویرایش پایانه',
-
     'Create New Terminal' => 'ایجاد پایانه جدید',
 
+    // Alerts
     'Terminal Not Found!' => "پایانه مورد نظر یافت نشد!",
-
     'Terminal Created Successfully.' => 'پایانه با موفقیت ویرایش شد.',
     'Terminal Edited Successfully.' => 'پایانه با موفقیت ویرایش شد.',
     'Terminal Deleted Successfully.' => 'پایانه با موفقیت حذف شد.',
 
-    // Form labels
-    'Name Label' => 'نام پایانه',
+    // Attributes
+    'Attributes' => [
+        'Name' => 'نام پایانه',
+    ],
 
     // Index/Table
     'Search Placeholder' => 'جستجو در پایانه‌ها...',
-    'Delete Confirmation' => 'آیا از حذف این پایانه مطمئن هستید؟',
     'No Terminals Found' => 'هیچ پایانه‌ای یافت نشد.',
+
+    // Delete
+    'Delete Confirmation' => 'آیا از حذف این پایانه مطمئن هستید؟',
 ];
